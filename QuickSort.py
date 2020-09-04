9a33b33addc7bff8a3e0d83d1b459d5b8101f111
parulsharma-121 <contactparulsharma11@gmail.com>
2020-09-04 20:11:41 +0530

    Quik Sort

---
+++ QuickSort.py
@@ -0,0 +1,28 @@
+'''
+Quick Sorting
+'''
+def partition(arr, low, high):
+    i = (low-1)        
+    pivot = arr[high]     
+    for j in range(low, high):
+        if(arr[j] <= pivot):
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+def quickSort(arr,low,high):
+    if(len(arr) == 1):
+        return arr
+    if(low < high):
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+
+arr = [2,5,3,98,34,66,21,34,67,43,23,89,90,65,43]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print(arr[i])
